508e61393c76e451c2d20ce6ec7fa6fd0d44249d
zhuyuankui <987672273@qq.com>
2019-07-22 22:51:01 +0800

    添加解决方案相关页面

--- src/main/java/com/yxkj/config/MyMvcConfig.java
+++ src/main/java/com/yxkj/config/MyMvcConfig.java
@@ -16,12 +16,28 @@ public class MyMvcConfig  extends WebMvcConfigurerAdapter{
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        //测试页面
+        registry.addViewController("/ceshi").setViewName("ceshi");
+        //首页
         registry.addViewController("/").setViewName("index");
-        registry.addViewController("/index").setViewName("ceshi");
+
+        //关于垣象
         registry.addViewController("/about").setViewName("about");
+        //核心技术
         registry.addViewController("/technology").setViewName("technology");
-        registry.addViewController("/product").setViewName("product");
+
+//        registry.addViewController("/product").setViewName("product");
+        //联系我们的页面
         registry.addViewController("/contact").setViewName("contact");
+
+        //安防领域页面
+        registry.addViewController("/security").setViewName("security");
+        //金融领域页面
+        registry.addViewController("/banking").setViewName("banking");
+        //交通领域页面
+        registry.addViewController("/city").setViewName("city");
+        //零售领域页面
+        registry.addViewController("/selling").setViewName("selling");
 
     }
 }
